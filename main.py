882d8dff64716b09156f4fca51c82c82594c2455
minusno0708 <naoyao1014@gmail.com>
2025-08-03 11:58:01 +0900

    feature: フォルダーが入力された際に再帰的に内部ファイルを処理

--- main.py
+++ main.py
@@ -26,36 +26,41 @@ def categorize_path(input_path, output_path):
     else:
         result[TYPE_KEY] = 'unsupported'
 
-    print(input_path.split('/')[1:])
-
     file_path = "/".join(input_path.split('/')[1:])
     result["output"] = os.path.join(output_path, file_path)
 
     return result
 
+def get_folder_elements(folder_path):
+    elements = []
+    for root, _, files in os.walk(folder_path):
+        for name in files:
+            elements.append(os.path.join(root, name))
+    return elements
+
 def run(input_path, output_path):
     path_category = categorize_path(input_path, output_path)
 
+    print(F"処理開始: {path_category['input']}")
+
     if path_category[TYPE_KEY] == 'folder':
-        # フォルダーの要素を取得して、子要素に対して再帰的に処理を行う
-        print("フォルダーの処理は未実装です")
-        pass
+        folder_elements = get_folder_elements(input_path)
+        for element in folder_elements:
+            run(element, output_path)
     elif path_category[TYPE_KEY] == 'video':
         print("動画ファイルの処理は未実装です")
     elif path_category[TYPE_KEY] == 'image':
         print("画像ファイルの処理は未実装です")
     else:
-        print("サポートされていないファイル形式です")
+        print(f"{path_category['input']} はサポートされていないファイル形式です")
 
 def main():
     if len(sys.argv) < 3:
-        print("Usage: python main.py <input_path> [<output_path>]")
+        print("Usage: python main.py <input_path> <output_path>")
         sys.exit(1)
 
     input_path = sys.argv[1]
     output_path = sys.argv[2]
-    print(f"入力: {input_path}")
-    print(f"出力: {output_path}")
 
     run(input_path, output_path)
 
